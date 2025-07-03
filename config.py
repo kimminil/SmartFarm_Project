85b3534a16046c6de45cf3f0e90107fc34226119
karist7 <rkdgns4566@naver.com>
2025-07-03 22:57:50 +0900

    setting

---
+++ config.py
@@ -0,0 +1,15 @@
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # 현재 디렉토리의 .env 파일 읽기
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
